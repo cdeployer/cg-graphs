ca1b74d9f47bb9bcdc5bbbed3e5eb431cf618ffa
Jarrod O'Malley <jarrod.omalley@cosive.com>
2018-01-03 13:34:46 +1100

    Updated filters for AS Source testing

--- src/components/annotationsFilter.js
+++ src/components/annotationsFilter.js
@@ -42,7 +42,7 @@ let annotationFormat = [{
 }];
 
 //filter annotation Country Comparison
-function annotationCountryComparisonFetchAndFilter(risk_id, country_code, asn) {
+function annotationFetchAndFilter(risk_id, country_code, asn) {
     return (
         fetch(`/static/scripts/publicAnnotation.json`)
         //change to /api/publicAnnotation.json for staging
@@ -51,36 +51,24 @@ function annotationCountryComparisonFetchAndFilter(risk_id, country_code, asn) {
         })
 
         .then((annData) => {
+            /* if annotation.risk_id is risk_id {
+                   if country_code and annotation.country_code is country_code {
+                          push annotation to list
+                   }
+                   if asn and annontation.asn is asn {
+                          push annotation to list
+                   }
+                   if not asn and not country_code {
+                          push annotation to list
+                   }
+               }
+               ....
+            } */
+
             console.log(annData);
             for (var ann_num in annData.notes) {
                 console.log(props.view);
                 if (annData.notes[ann_num].risk_id == risk_id) {
-                    console.log(ann_num);
-                    console.log(annData.notes[ann_num].annotation_date);
-                    annotation_dates.push(annData.notes[ann_num].annotation_date);
-                    annotation_notes.push(annData.notes[ann_num].annotation_date + '\n' + annData.notes[ann_num].annotation);
-                    annotations.push({
-                        return(annotationFormat)
-                    })
-                }
-            }
-        }));
-}
-
-//filter annotation AS Source
-function annotationAsSourceFilter(props) {
-    return (
-        fetch(`/static/scripts/publicAnnotation.json`)
-        //change to /api/publicAnnotation.json for staging
-        .then((response) => {
-            return response.json()
-        })
-
-        .then((annData) => {
-            console.log(annData);
-            for (var ann_num in annData.notes) {
-                console.log(props.view);
-                if (annData.notes[ann_num].risk_id == props.view.risk) {
                     console.log(ann_num);
                     console.log(annData.notes[ann_num].annotation_date);
                     annotation_dates.push(annData.notes[ann_num].annotation_date);
